4fc5e0c17d655ef9ef51ba8df0b6737e39ea700b
John Doe <john.doe@example.com>
2024-09-05 16:31:35 +0530

    when the user tooks a1 train ticket the user id and booking id will get saved in 1the object

--- src/Components/Pages/Trains/Cards/card.tsx
+++ src/Components/Pages/Trains/Cards/card.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { TrainDataProps } from "../type";
-import './card.css'
+import './card.css';
 
 const TrainCard: React.FC<TrainDataProps> = ({
   trainName,
@@ -11,37 +11,40 @@ const TrainCard: React.FC<TrainDataProps> = ({
   imageURL,
   onButtonClick,
   buttonText,
+  onchange
 }) => {
   return (
-   <div className="cardlayout">
-     <div className="bg-white shadow-lg rounded-lg mb-4 overflow-hidden">
-      <div className="flex">
-        <img
-          src={imageURL}
-          alt="Train"
-          className="w-1/3 h-full object-cover"
-        />
-        <div className="w-2/3 p-6 flex flex-col justify-between">
-          <div>
-            <h2 className="text-2xl font-bold text-gray-800">{trainName}</h2>
-            <p className="text-lg text-gray-600 mt-2">{date}</p>
-            <p className="text-lg text-gray-600 mt-2">{arrival}</p>
-            <p className="text-lg text-gray-600 mt-2">{departure}</p>
-            <p className="text-sm text-gray-500 mt-4">{price}</p>
-          </div>
-          <div className="mt-4">
-            <button
-              className="bg-green-600 text-white py-2 px-4 rounded"
-              onClick={onButtonClick}
-            >
-              {buttonText}
-            </button>
+    <div className="cardlayout">
+      <div className="bg-white shadow-lg rounded-lg mb-4 overflow-hidden">
+        <div className="flex">
+          <img
+            src={imageURL}
+            alt="Train"
+            className="w-1/3 h-full object-cover"
+          />
+          <div className="w-2/3 p-6 flex flex-col justify-between">
+            <div>
+              <h2 className="text-2xl font-bold text-gray-800">{trainName}</h2>
+              <p className="text-lg text-gray-600 mt-2">{date}</p>
+              <p className="text-lg text-gray-600 mt-2">{arrival}</p>
+              <p className="text-lg text-gray-600 mt-2">{departure}</p>
+              <p className="text-sm text-gray-500 mt-4">{price}</p>
+            </div>
+            <div className="mt-4">
+              <button
+                className="bg-gradient-to-r from-green-400 via-green-500 to-green-600 text-white py-2 px-4 rounded transition-transform transform hover:scale-105 hover:opacity-90 focus:outline-none"
+                onClick={onButtonClick}
+                onMouseEnter={onchange} // Trigger onchange on button hover
+              >
+                {buttonText}
+              </button>
+            </div>
           </div>
         </div>
       </div>
     </div>
-   </div>
   );
 };
 
 export default TrainCard;
+
